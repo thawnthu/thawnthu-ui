2866e7b60532defd14f01b4be2b71cf3e4ec33a9
thawnthu <thawn.thu95@gmail.com>
2026-08-18 15:07:49 +0530

    Create page.tsx

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <main style={{padding: '20px', paddingBottom: '70px'}}>
+      <h2>About Thawnthu</h2>
+      <p>He website hi Mizo thawnthu chhiarna leh ziaktute tan siam ani.</p>
+      <p>Version: 1.0</p>
+    </main>
+  )
+}
