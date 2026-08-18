176a87320677835cd42c5db3f636f727dc7304ba
thawnthu <thawn.thu95@gmail.com>
2026-08-18 13:22:20 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,16 @@
-export const metadata = { title: 'Thawnthu App' }
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return <html lang="en"><body style={{margin: 0}}>{children}</body></html>
+export const metadata = {
+  title: 'Thawnthu App',
+  description: 'Mizo Thawnthu chhiarna',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body style={{margin: 0, fontFamily: 'Arial'}}>{children}</body>
+    </html>
+  )
 }
