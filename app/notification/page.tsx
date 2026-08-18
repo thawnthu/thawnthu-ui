1e47bf202e941846b418556e84a35c8985c5abe5
thawnthu <thawn.thu95@gmail.com>
2026-08-18 15:07:13 +0530

    Create page.tsx

---
+++ app/notification/page.tsx
@@ -0,0 +1,8 @@
+export default function NotificationPage() {
+  return (
+    <main style={{padding: '20px', paddingBottom: '70px'}}>
+      <h2>Notification</h2>
+      <p>Post thar leh Comment thar notification hi hna thawh mek ani</p>
+    </main>
+  )
+}
