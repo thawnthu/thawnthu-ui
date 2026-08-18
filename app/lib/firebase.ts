00b1171d5edfd4ac562db75f495894a9ac3f27d3
thawnthu <thawn.thu95@gmail.com>
2026-08-18 13:33:59 +0530

    Update firebase.ts

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -1,16 +1,4 @@
 import { initializeApp, getApps } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCYP1iIoI7KsJIcw0mP0s6_EYZ8JWmIQXU",
-  authDomain: "thawnthu-e3b68.firebaseapp.com",
-  projectId: "thawnthu-e3b68",
-  storageBucket: "thawnthu-e3b68.firebasestorage.app",
-  messagingSenderId: "580550703112",
-  appId: "1:580550703112:web:9a67fc09b9150aa20ff9bd"
-};
-
+const firebaseConfig = { apiKey: "TEST", projectId: "TEST" };
 const app =!getApps().length? initializeApp(firebaseConfig) : getApps()[0];
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+export default app;
