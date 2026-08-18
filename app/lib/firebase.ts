17516d9f17dadae438f184195999293d6f094b8d
thawnthu <thawn.thu95@gmail.com>
2026-08-18 13:40:45 +0530

    Update firebase.ts

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -1,4 +1,4 @@
 import { initializeApp, getApps } from "firebase/app";
-const firebaseConfig = { apiKey: "TEST", projectId: "TEST" };
+const firebaseConfig = { apiKey: "AIzaSyCYP1iIoI7KsJIcw0mP0s6_EYZ8JWmIQXU", projectId: "thawnthu-e3b68" };
 const app =!getApps().length? initializeApp(firebaseConfig) : getApps()[0];
 export default app;
