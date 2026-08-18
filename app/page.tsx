55e9021ebb506cb77ba6ebd3d4dc45168174b907
thawnthu <thawn.thu95@gmail.com>
2026-08-18 13:18:46 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
-      <h1>Thawnthu App A Tla Tawh! 🎉</h1>
+      <h1>Thawnthu App A Tla Tawh!</h1>
     </main>
   )
-}}
+}
