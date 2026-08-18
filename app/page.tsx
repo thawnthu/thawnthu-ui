360cdf266da756d5810b58ecd3cde28f417d128d
thawnthu <thawn.thu95@gmail.com>
2026-08-18 18:25:16 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+'use client'
+import { useState } from 'react';
+
 export default function HomePage() {
+  const [msg] = useState('Thawnthu a tluang e ✅')
   return (
     <main style={{padding: '20px', background: '#f8f9fa', minHeight: '100vh'}}>
-      <h1 style={{fontSize: '24px', fontWeight: '800'}}>Thawnthu a tluang e ✅</h1>
-      <p>Hei chiah hi a lang thei chuan kan code dang ah buaina a awm tihna</p>
+      <h1 style={{fontSize: '24px', fontWeight: '800'}}>{msg}</h1>
     </main>
   )
 }
