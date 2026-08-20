c8c63b26fe4163762f900587e2e26aa344275b3a
thawnthu <thawn.thu95@gmail.com>
2026-08-20 15:07:12 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function HomePage() {
     textAlign: 'left',
     display: 'flex',
     alignItems: 'center',
-    gap: '16px',
+    gap: '20px',
     fontSize: '15px',
     cursor: 'pointer',
     color: text,
