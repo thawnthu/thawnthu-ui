b7441783557ef7b20b6784b20172d967914f474d
thawnthu <thawn.thu95@gmail.com>
2026-08-21 11:28:13 +0530

    Update page.tsx

--- app/forgot/page.tsx
+++ app/forgot/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useState } from 'react';
 import { sendPasswordResetEmail } from "firebase/auth";
-import { auth } from "../lib/firebase";
+import { auth } from "@/lib/firebase";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 
