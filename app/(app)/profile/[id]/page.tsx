8940b02a4ecad1a38564fce77654fe84fed3d517
thawnthu <thawn.thu95@gmail.com>
2026-08-22 18:40:24 +0530

    Update page.tsx

--- app/(app)/profile/[id]/page.tsx
+++ app/(app)/profile/[id]/page.tsx
@@ -210,4 +210,4 @@ export default function ProfilePage() {
       </div>
     </div>
   );
-                                                                                                                                                                   }
+}
